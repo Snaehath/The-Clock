6a5626eb696a306bf47d91fe310ced75695c1e46
Snaehath <2020ad0556@svce.ac.in>
2025-07-20 20:04:03 +0530

    chore: update code

--- src/components/Quote.tsx
+++
@@ -1,3 +0,0 @@
-export default function Day(){
-    return <p>There are two types of Pain, One that hurt and one that changes you</p>
-}
--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 
-export default function Time(){
-    const [currTime, setCurrTime] = useState<Date>(new Date());
-   
-    useEffect(() => {
-        setInterval(() => {
-            setCurrTime(new Date());
-        }, 1000);
-    }, [setCurrTime]);
+export default function Time() {
+  const [currTime, setCurrTime] = useState<Date>(new Date());
 
-    let hours = currTime.getHours();
-    let text  = hours > 11 ? "PM": "AM";
-    hours = hours > 12 ? hours-12: hours;
+  useEffect(() => {
+    setInterval(() => {
+      setCurrTime(new Date());
+    }, 1000);
+  }, [setCurrTime]);
 
-    return (
+  let hours = currTime.getHours();
+  let text = hours > 11 ? "PM" : "AM";
+  hours = hours > 12 ? hours - 12 : hours;
+
+  return (
     <div className="main">
-        <div/>
-        <div className="quote">
-            <p>"Everyone is a Hunter until Lion Comes to play!"</p>
-        </div>
-        <div>
-            <span>{hours.toString().padStart(2, '0')}:</span><span>{currTime.getMinutes().toString().padStart(2,'0')}</span><span className="sec">.{currTime.getSeconds().toString().padStart(2,'0')}</span><span className="session">{text}</span>
-        </div>
-        <div className="date">
-            <span className="Date">{currTime.toDateString()}</span>
-        </div>
+      <div />
+      <div>
+        <span>{hours.toString().padStart(2, "0")}:</span>
+        <span>{currTime.getMinutes().toString().padStart(2, "0")}</span>
+        <span className="sec">
+          .{currTime.getSeconds().toString().padStart(2, "0")}
+        </span>
+        <span className="session">{text}</span>
+      </div>
+      <div className="date">
+        <span className="Date">{currTime.toDateString()}</span>
+      </div>
     </div>
-   );
+  );
 }
